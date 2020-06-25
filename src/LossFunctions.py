9a84b5584d9d27c02abbbde98d8b75a2b312f53e
Maitrey <maitrey2112@gmail.com>
2020-06-25 14:39:45 -0400

    Loss functions working, optimizer not created yet, generated random image

---
+++ src/LossFunctions.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+
+
+def content_loss(original_activation, generated_activation):
+
+    # Activation shape = (1, 31, 31, 512)
+    m, n_H, n_W, n_C = original_activation.get_shape().as_list()
+
+    # Reshaping activations
+    # a_C_unrolled = tf.reshape(original_activation, shape=(-1, n_C))
+    # a_G_unrolled = tf.reshape(generated_activation, shape=(-1, n_C))
+
+    loss = (1 / (4 * n_H * n_W * n_C)) * tf.reduce_sum(tf.square(tf.subtract(original_activation, generated_activation)))
+    return loss
+
+
+def gram_matrix(activation):
+
+    activation = tf.matmul(activation, tf.transpose(activation))
+    return activation
+
+
+def layer_style_loss(original_activation, generated_activation):
+
+    m, n_H, n_W, n_C = original_activation.get_shape().as_list()
+
+    # Reshape the images to have them of shape (n_C, n_H*n_W)
+    a_S = tf.transpose(tf.reshape(original_activation, shape=(-1, n_C)))
+    a_G = tf.transpose(tf.reshape(generated_activation, shape=(-1, n_C)))
+
+    # gram matrices
+    GS = gram_matrix(a_S)
+    GG = gram_matrix(a_G)
+
+    # Computing the loss
+    loss = 1 / (4 * n_C ** 2 * (n_H * n_W) ** 2) * tf.reduce_sum((GS - GG) ** 2)
+    return loss
+
+def overall_style_loss(layer_weights, original_activations, generated_activations):
+
+    total_style_loss = 0
+
+    for index in range(len(layer_weights)):
+        weight = layer_weights[index]
+        loss = layer_style_loss(original_activations[index], generated_activations[index])
+        total_style_loss += (weight * loss)
+
+    return total_style_loss
+
+def total_loss(layer_weights, content_activations, style_activations, generated_activations, alpha=0.5, beta=0.5):
+
+    c_loss = content_loss(content_activations[-1], generated_activations[-1])
+    s_loss = overall_style_loss(layer_weights, style_activations[:-1], generated_activations[:-1])
+
+    print(c_loss, s_loss)
+
+    t_loss = alpha * c_loss + beta * s_loss
+
+    return t_loss
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -1,32 +1,42 @@
-import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import PIL
 import h5py
-from load_vgg_model import model_class
-from updatedModel import custom_model
+from LoadVggModel import model_class
+from UpdatedModel import custom_model
+from LossFunctions import total_loss
+from noisy_image import generate_noisy_image
 
+STYLE_WEIGHTS = [0.2] * 5
 
 def get_class_model():
     vgg_m = model_class()
     return vgg_m
 
-## Loading Image
-## How to normalize it properly ???
+
+# How to normalize it properly ???
 def load_image(image_path):
-    return np.array(keras.preprocessing.image.load_img(image_path, target_size=(500,500)), dtype='float32').reshape(1,500,500,3) / 255.
+    image = np.array(keras.preprocessing.image.load_img(image_path, target_size=(500,500)), dtype='float32').reshape(1,500,500,3) / 255.
+    return image
 
 
-modelClass = get_class_model() ## Model Class
-vgg_model = modelClass.return_model() ## Actual Model
+modelClass = get_class_model() # Model Class
+vgg_model = modelClass.return_model() # Actual Model
 vgg_weights = modelClass.get_all_weights()
 
 input_image = load_image('1.jpg')
+style_image = load_image('2.jpg')
+random_image = generate_noisy_image(input_image)
+
 
 custom_model1 = custom_model(vgg_model, vgg_weights)
 
-custom_output = custom_model1(input_image)
-print('{} \n {}'.format(custom_output, len(custom_output)))
+custom_output_content = custom_model1(input_image)
+custom_output_style = custom_model1(style_image)
+custom_random_image = custom_model1(random_image)
+
+tl = total_loss(STYLE_WEIGHTS, custom_output_content, custom_output_style, custom_random_image)
 
 
+print(tl)
